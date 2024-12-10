7a4a052af52e861f521a00a2b57cb6d600a51c80
Bruno <brunoplay055@gmail.com>
2024-12-10 11:01:05 -0300

    feat: create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div className="flex items-center justify-around p-10">
+        <h1 className="text-4xl">EnduraFit</h1>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/product" className="text-xl">
+              Top 10
+            </Link>
+          </li>
+          <li>
+            <Link href="/product" className="text-xl">
+              Lançamentos
+            </Link>
+          </li>
+          <li>
+            <Link href="/product" className="text-xl">
+              Whey Protein
+            </Link>
+          </li>
+          <li>
+            <Link href="/product" className="text-xl">
+              Objetivos
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/product"
+              className="text-xl bg-foreground text-background py-2 px-4 rounded-full"
+            >
+              Comprar agora
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
